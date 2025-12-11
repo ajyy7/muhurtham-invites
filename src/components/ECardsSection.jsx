3a4ef3dd89b54e35a2879bd0ff9d0b7711ff960d
krishnapriya n <krishnapriyanandan2000@gmail.com>
2025-12-11 16:14:58 +0530

    Added E-cards

--- src/components/ECardsSection.jsx
+++ src/components/ECardsSection.jsx
@@ -8,6 +8,15 @@ import ecard04 from "../assets/ecards/ecard04.png";
 import ecard05 from "../assets/ecards/ecard05.png";
 import ecard06 from "../assets/ecards/ecard06.png";
 import ecard07 from "../assets/ecards/ecard07.png";
+import ecard08 from "../assets/ecards/ecard08.jpg";
+import ecard09 from "../assets/ecards/ecard09.jpg";
+import ecard10 from "../assets/ecards/ecard10.png";
+import ecard11 from "../assets/ecards/ecard11.png";
+import ecard12 from "../assets/ecards/ecard12.png";
+import ecard13 from "../assets/ecards/ecard13.png";
+import ecard14 from "../assets/ecards/ecard14.png";
+import ecard15 from "../assets/ecards/ecard15.png";
+import ecard16 from "../assets/ecards/ecard16.png";
 
 const ECardsSection = () => {
   const ecards = [
@@ -17,7 +26,16 @@ const ECardsSection = () => {
     { id: "04", src: ecard04 },
     { id: "05", src: ecard05 },
     { id: "06", src: ecard06 },
-    { id: "07", src: ecard07 }
+    { id: "07", src: ecard07 },
+    { id: "08", src: ecard08 },
+    { id: "09", src: ecard09 },
+    { id: "10", src: ecard10 },
+    { id: "11", src: ecard11 },
+    { id: "12", src: ecard12 },
+    { id: "13", src: ecard13 },
+    { id: "14", src: ecard14 },
+    { id: "15", src: ecard15 },
+    { id: "16", src: ecard16 }
   ];
 
   return (
